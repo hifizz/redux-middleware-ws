4d8f83c6ee75d702b5bd08899c86a66bf1508852
Stack fizz <huangchaolin@xylink.com>
2018-06-22 11:52:24 +0800

    feat: add type define & add util functions.

--- src/redux-middleware-ws.ts
+++ src/redux-middleware-ws.ts
@@ -1,44 +1,59 @@
 export const WEBSOCKET_CONNECT = 'WEBSOCKET_CONNECT'
+export const WEBSOCKET_OPEN = 'WEBSOCKET_OPEN'
+export const WEBSOCKET_CLOSE = 'WEBSOCKET_CLOSE'
+export const WEBSOCKET_MESSAGE = 'WEBSOCKET_MESSAGE'
 export const WEBSOCKET_SEND = 'WEBSOCKET_SEND'
 export const WEBSOCKET_DISCONNECT = 'WEBSOCKET_DISCONNECT'
 
-export interface IWebsocket {
+const webSocketSet: {
   [key: string]: WebSocket
+} = {}
+
+const defaultIdentity = '__default__'
+
+function createActionType(actionPrefix: string, identity: string | number): string {
+  return identity === defaultIdentity ? `${actionPrefix}_${identity}` : actionPrefix
 }
 
-const webSocketSet: IWebsocket = {}
-
-function createActionType(actionPrefix: string, identity: string | number) {
-  return identity ? `${actionPrefix}_${identity}` : actionPrefix
+function getIdentity(action: any): string {
+  return (action.payload && action.payload.identity) || defaultIdentity
 }
 
 export const webSocketMiddleware = (store: any) => (next: any) => (action: any) => {
-  const index: string = action.payload.identity || 'index'
+  const identity: string = getIdentity(action)
+
   switch (action.type) {
     case WEBSOCKET_CONNECT:
-      webSocketSet[index] = new WebSocket(action.payload.url)
-      webSocketSet[index].onopen = () => {
-        store.dispatch({ type: `WEBSOCKET_OPEN__${action.payload.wsName}` })
+      webSocketSet[identity] = new WebSocket(action.payload.url)
+      webSocketSet[identity].onopen = () => {
+        store.dispatch({
+          type: createActionType(WEBSOCKET_OPEN, identity)
+        })
         if (action.payload.onOpen) {
-          action.payload.onOpen(webSocketSet[index])
+          action.payload.onOpen(webSocketSet[identity])
         }
       }
-      webSocketSet[index].onclose = () => {
-        store.dispatch({ type: `WEBSOCKET_CLOSE_${action.payload.wsName}` })
+      webSocketSet[identity].onclose = () => {
+        store.dispatch({
+          type: createActionType(WEBSOCKET_CLOSE, identity)
+        })
       }
 
-      webSocketSet[index].onmessage = (message: any) => {
-        store.dispatch({ type: `WEBSOCKET_MESSAGE_${action.payload.wsName}`, payload: { message } })
+      webSocketSet[identity].onmessage = (message: any) => {
+        store.dispatch({
+          type: createActionType(WEBSOCKET_MESSAGE, identity),
+          payload: { message }
+        })
       }
       break
 
     case WEBSOCKET_SEND:
-      webSocketSet[index].send(action.payload.datas)
+      webSocketSet[identity].send(action.payload.datas)
       break
 
     case WEBSOCKET_DISCONNECT:
-      if (webSocketSet[index]) {
-        webSocketSet[index].close()
+      if (webSocketSet[identity]) {
+        webSocketSet[identity].close()
       }
       break
 
